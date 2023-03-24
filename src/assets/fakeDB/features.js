e7afa28e111645d7b0cea2c1a2dc9a340dfa5a36
Mufadal03 <sadrimufadal@gmail.com>
2023-03-24 14:33:31 +0530

    dummy data for features

---
+++ src/assets/fakeDB/features.js
@@ -0,0 +1,27 @@
+export const Feautures = [
+    {
+        id: 1,
+        IconColor: 'linear-gradient(135deg, #3BC9FB 0%, #42A6FB 100%)',
+        bgColor: '#EEFEFD',
+        borderColor:'#D4EAE8'
+    },
+    {
+        id: 2,
+        IconColor: 'linear-gradient(135deg, #72E6E5 0%, #75DFF0 100%)',
+        bgColor: '#EEFEF0',
+        borderColor:'#CEE5DF'
+    },
+    {
+        id: 3,
+        IconColor: 'linear-gradient(135deg, #80FC6A 0%, #41F064 100%)',
+        bgColor: '#EFFAF6   ',
+        borderColor:'#CEDAC2'
+    },
+    {
+        id: 4,
+        IconColor: 'linear-gradient(135deg, #2E73E8 0%, #215FDA 100%)',
+        bgColor: '#F0F7FF',
+        borderColor:'#C7DEE6'
+    }
+
+]
