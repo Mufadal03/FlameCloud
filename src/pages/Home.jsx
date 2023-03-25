632d9591df08b46632a89e433f952a2832293dca
Mufadal03 <sadrimufadal@gmail.com>
2023-03-25 14:47:30 +0530

    home and proxy location checker added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,20 @@
+import { Box } from '@chakra-ui/react'
 import React from 'react'
+import Blog from '../components/Blog'
+import Features from '../components/Features'
+import Landing from '../components/Landing'
+import Plans from '../components/Plans'
+import Testimonial from '../components/Testimonial'
 
 const Home = () => {
   return (
-    <div>Home</div>
+      <Box overflow={'hidden'} >
+          <Landing />
+          <Features />
+          <Plans />
+          <Testimonial />
+          <Blog />
+    </Box>
   )
 }
 
